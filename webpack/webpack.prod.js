d4b361db58c7a250a9d7c95c1a39afa365ef3efa
Duc Doan <ducdhm@gmail.com>
2020-11-29 23:48:07 +0700

    Update

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,34 +2,17 @@ const packageJson = require('../package.json');
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ZipPlugin = require('zip-webpack-plugin');
+const ZipFilesPlugin = require('webpack-zip-files-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
-        new ZipPlugin({
-            path: '../',
-
-            filename: `${packageJson.name}-${[packageJson.version]}.zip`,
-
-            pathMapper: function(assetPath) {
-                if (!assetPath.endsWith('.js')) {
-                    return path.basename(assetPath);
-                }
-
-                return assetPath;
-            },
-
-            fileOptions: {
-                mtime: new Date(),
-                mode: 0o100664,
-                compress: true,
-                forceZip64Format: false,
-            },
-
-            zipOptions: {
-                forceZip64Format: false,
-            },
-        })
+        new ZipFilesPlugin({
+            entries: [
+                { src: path.join(__dirname, './dist/') },
+            ],
+            output: path.join(__dirname, './dist/', `${packageJson.name}-${[packageJson.version]}`),
+            format: 'zip',
+        }),
     ]
 });
