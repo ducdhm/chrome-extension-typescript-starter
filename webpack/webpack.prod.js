3ca27745987bf41efe20836cbee20d2fb83577c2
Duc Doan <ducdhm@gmail.com>
2020-12-01 22:41:10 +0700

    Zip file after build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,17 +2,32 @@ const packageJson = require('../package.json');
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ZipFilesPlugin = require('webpack-zip-files-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
-        new ZipFilesPlugin({
-            entries: [
-                { src: path.join(__dirname, './dist/') },
-            ],
-            output: path.join(__dirname, './dist/', `${packageJson.name}-${[packageJson.version]}`),
-            format: 'zip',
+        new FileManagerPlugin({
+            events: {
+                onEnd: {
+                    archive: [
+                        {
+                            source: path.join(__dirname, '../dist/'),
+                            destination: path.join(__dirname, '../dist/', `${packageJson.name}-${packageJson.version}.zip`),
+                            format: 'zip',
+                            options: {
+                                gzip: true,
+                                gzipOptions: {
+                                    level: 1,
+                                },
+                                globOptions: {
+                                    nomount: true,
+                                },
+                            },
+                        },
+                    ],
+                },
+            },
         }),
-    ]
+    ],
 });
